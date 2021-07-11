caf99163530bc7ef3871977f5fc15cc6b4cfa784
Claudio <claudiojlf@gmail.com>
2021-07-11 14:06:56 -0300

    small adjustments

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ const menu = document.querySelectorAll('header .menu')
 
 for (const button of menuButtons) {
 	button.addEventListener('click', function () {
-		toggleMenu()
+		for (const menuItem of menu) {
+			menuItem.classList.toggle('show')
+		}
 	})
-}
-
-function toggleMenu() {
-	for (const menuItem of menu) {
-		menuItem.classList.toggle('show')
-	}
 }
 
 //close menu when selecting option
@@ -19,6 +15,24 @@ const links = document.querySelectorAll('header nav ul li a')
 
 for (const link of links) {
 	link.addEventListener('click', function () {
-		toggleMenu()
+		for (const menuItem of menu) {
+			menuItem.classList.remove('show')
+		}
 	})
 }
+
+//header transparent when on top
+const header = document.querySelector('header')
+
+function headerOnTop() {
+	if (window.scrollY == 0) {
+		header.classList.add('onTop')
+	} else {
+		header.classList.remove('onTop')
+	}
+}
+
+//scrolling
+window.addEventListener('scroll', function () {
+	headerOnTop()
+})
